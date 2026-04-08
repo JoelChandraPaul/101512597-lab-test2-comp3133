3dc54e2509b1bc1c1683500b8d48814139dd9390
Joel Chandra Paul <joelcpaul02@gmail.com>
2026-04-08 09:33:12 -0400

    Lab Test Completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Missionlist } from './missionlist/missionlist';
+import { Missiondetails } from './missiondetails/missiondetails';
 
 export const routes: Routes = [
-  { path: '', component: Missionlist }
+  { path: '', component: Missionlist },
+  { path: 'mission/:id', component: Missiondetails }
 ];
--- src/app/missiondetails/missiondetails.ts
+++ src/app/missiondetails/missiondetails.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { SpacexService } from '../services/spacex';
+import { Mission } from '../models/mission';
 
 @Component({
   selector: 'app-missiondetails',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './missiondetails.html',
-  styleUrl: './missiondetails.css',
+  styleUrl: './missiondetails.css'
 })
-export class Missiondetails {}
+export class Missiondetails implements OnInit {
+  private route = inject(ActivatedRoute);
+  private spacexService = inject(SpacexService);
+
+  mission = signal<Mission | null>(null);
+  loading = signal(true);
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.spacexService.getMissionByFlightNumber(+id).subscribe((data) => {
+        this.mission.set(data);
+        this.loading.set(false);
+      });
+    }
+  }
+}
